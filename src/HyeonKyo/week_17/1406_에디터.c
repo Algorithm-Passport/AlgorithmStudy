9b82ca8424fe2f8d073926f756aa71e22247d657
Hyeonkyo <zkdlajt96@naver.com>
2021-06-22 01:35:37 +0900

    editor ing

--- src/HyeonKyo/week_17/1406_에디터.c
+++ src/HyeonKyo/week_17/1406_에디터.c
@@ -7,64 +7,129 @@ typedef struct		s_node
 	char	data;
 	struct s_node *prev;
 	struct s_node *next;
-}					t_node;
+}					node;
 
-void	create_node(t_node **lst)
+node	*create_node(char c)
 {
-	*lst = (t_node *)malloc(sizeof(*lst));
-	memset(*lst, 0, sizeof(*lst));
+	node *lst;
+	lst = (node *)malloc(sizeof(lst));
+	memset(lst, 0, sizeof(lst));
+	lst->data = c;
+	return (lst);
 }
 
-void	next(t_node **lst, t_node *tmp)
+void	link_node(node *cur)
 {
-	t_node tmp;
+	node *tmp;
+	tmp = create_node(0);
+	cur->next = tmp;
+	tmp->prev = cur;
+}
 
-	create_node(&(*lst->prev));
-	*lst->prev = tmp;
-	tmp->next = *lst;
-	create_node(&(*lst->next));
-	tmp = *lst->next;
-	tmp->prev = *lst;
-	*lst = *(lst->next);
+void	insert_node(node *cur, char c)
+{
+	node *tmp;
+	node *new;
+
+	cur->prev = tmp;
+	new = create_node(c);
+	cur->prev = new;
+	new->next = cur;
+	tmp->next = new;
+	new->prev = tmp;
+}
+
+void	edit_L(node **cur)
+{
+	if ((*cur)->prev != 0)
+		*cur = (*cur)->prev;
+}
+
+void	edit_D(node **cur)
+{
+	if ((*cur)->next != 0)
+		*cur = (*cur)->next;
+}
+
+void	edit_B(node **cur)
+{
+	node *front;
+	node *tmp;
+
+	if ((*cur)->prev == 0)
+		return ;
+	tmp = (*cur)->prev;
+	front = tmp->prev;
+	(*cur)->prev = front;
+	front->next = (*cur);
+	free(tmp);
+	tmp = 0;
+}
+
+void	edit_P(node *cur, char c)
+{
+	insert_node(cur, c);
+}
+
+void	del_lst(node *head)
+{
+	node *tmp;
+	
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
 
 int main()
 {
-	char	*tmp;
-	t_node	*lst;
-	t_node	*head;
-	t_node	*tmp;
-	t_node	*cs;
-	int		M, i, len, idx, max = 0;
-	char	c[1000001];
+	char	str[100001];
+	node	*cur;
+	node	*head;
+	node	*tmp;
+	int		M, i, j = 0;
+	char	c1[1000001];
+	char	c2[1000001];
 
-	create_node(&head);
-	create_node(&lst);
-	create_node(&tmp);
-	head = lst;
-	tmp = lst;
-	next(&lst, tmp);
-	while (1)
+	head = create_node(0);
+	cur = create_node(0);
+	head->next = cur;
+	cur->prev = head;
+	scanf("%s\n", str);
+	while (str[i])
 	{
-		scanf("%c", lst->data);
-		if (lst->data == 0)
-			break ;
-		next(&lst, tmp);
+		cur->data = str[i++];
+		link_node(cur);
+		cur = cur->next;
 	}
-	cs = lst;
-	scanf("%d", &M);
+	scanf("%d\n", &M);
 	for (i = 0; i < M; i++)
 	{
-		scanf("%c", &(c[i]));
-		if (c[i] == 'P')
-			scanf("%c", &(c[++i]));
+		scanf("%c ", c1 + i);
+		if (c1[i] == 'P')
+			scanf("%c\n", c2 + j++);
 	}
-	/*
-	1. cs에 각 명령들 적용
-		1. L
-		2. D
-		3. B
-		4. P $
-	*/
+	j = 0;
+	for (i = 0; i < M; i++)
+	{
+		if (c1[i] == 'L')
+			edit_L(&cur);
+		else if (c1[i] == 'D')
+			edit_D(&cur);
+		else if (c1[i] == 'B')
+			edit_B(&cur);
+		else if (c1[i] == 'P')
+			edit_P(cur, c2[j++]);
+	}
+	cur = head->next;
+	while (cur != 0)
+	{
+		printf("%c", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+	del_lst(head);
 	return (0);
 }
