cdd24bcace7fdb43f99813207df8b0b05e2d8e47
lotus0204 <lotusrb0204@gmail.com>
2022-01-25 19:56:05 +0900

    SangYeop<22.01.25>

--- src/SangYeop/9342.py
+++ src/SangYeop/9342.py
@@ -32,3 +32,32 @@ for _ in range(N):
         return 
     print('Infected!')
   solution(T)
+
+# ##############
+import sys
+
+T = int(sys.stdin.readline())
+rule = ['A', 'B', 'C', 'D', 'E', 'F', '']
+for _ in range(T):
+    s = sys.stdin.readline().rstrip()
+    i = 0
+    stack = []
+    for c in s:
+        if stack:
+            if stack[-1] == c:
+                continue
+        stack.append(c)
+    short = ''.join(stack)
+    if 'A' not in short:
+        print('Good')
+    else:
+        start = short.index('A')
+        if short[start:start+3] != 'AFC':
+            print('Good')
+        else:
+            left = short[:start]
+            right = short[start+3:]
+            if left in rule and right in rule:
+                print("Infected!")
+            else:
+                print('Good')
